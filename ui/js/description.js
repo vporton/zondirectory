0f04e30f6e6397dde08772683cd946be984542ea
Victor Porton <porton@narod.ru>
2020-07-02 19:24:13 +0300

    decription.js

--- ui/js/description.js
+++ ui/js/description.js
@@ -3,13 +3,12 @@
 async function createNewItem() {
     const contract = web3.eth.contract(await categoriesJsonInterface());
     const contractInstance = contract.at(categoriesContractAddress);
-    const title = document.getElementById('title');
-    const short = document.getElementById('short');
-    const long = document.getElementById('long');
-    contractInstance.createItem.call(title, short, long, { from: web3.eth.accounts[0]})
-        .then(receipt => {
-            const event = receipt.events.ItemUpdated;
-            const itemId = event.returnValues.id;
-            open('upload.html?id=' + itemId);
-        });
+    const title = document.getElementById('title').value;
+    const short = document.getElementById('short').value;
+    const long = document.getElementById('long').value;
+    contractInstance.createItem(title, short, long, {gas: '5000000'}, receipt => {
+        const event = receipt.events.ItemUpdated;
+        const itemId = event.returnValues.id;
+        open('upload.html?id=' + itemId);
+    });
 }
