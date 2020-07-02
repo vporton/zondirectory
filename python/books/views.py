cc2435fe463b19e5c3df8cc9f0435d01dcdd055e
Victor Porton <porton@narod.ru>
2020-07-02 13:42:09 +0300

    deleted python/

--- python/books/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-from web3.auto import w3
-
-
-class BooksListView(View):
-    def get(self, request):
-        w3.
-        books =
