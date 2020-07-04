206cb4a4661b5a5871709c751e088baa26fafa9d
Victor Porton <porton@narod.ru>
2020-07-04 17:54:00 +0300

    retrieving author's items

--- ui/js/author.js
+++ ui/js/author.js
@@ -1,13 +1,23 @@
 async function myBooks() {
     await defaultAccountPromise();
-        query = `{
+    let query = `{
     setItemOwners(orderBy:id, orderDirection:desc, where:{owner:"${defaultAccount}"}) {
         itemId    
     }
 }`;
     let itemIds = (await queryThegraph(query)).data.setItemOwners;
-    console.log(itemIds)
-    //itemIds = new Set(itemIds);
+    itemIds = itemIds.filter((x, i, a) => a.indexOf(x) == i); // unique values
+    function itemReq(itemId) {
+        return `itemUpdateds(first:1, orderBy:itemId, orderDirection:desc, where:{itemId:${itemId}}) {
+            itemId
+            title
+            priceETH
+            priceAR
+        }`;
+    }
+    query = "{\n" + itemIds.map(itemId => itemReq(itemId)).join("\n") + "\n}";
+    let items = (await queryThegraph(query)).data.itemUpdateds;
+    console.log(items)
 }
 
 $(myBooks);
