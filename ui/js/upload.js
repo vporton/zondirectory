6d3107889fe4e930b3007d0ae9814161652ee539
Victor Porton <porton@narod.ru>
2020-07-04 21:32:18 +0300

    uploads.js

--- ui/js/upload.js
+++ ui/js/upload.js
@@ -1,4 +1,8 @@
 async function upload() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const itemId = urlParams.get('id');
+    if(!itemId) return; // just to be sure
+
     const arweave = Arweave.init({
         host: 'arweave.net',// Hostname or IP address for a Arweave host
         port: 443,          // Port
@@ -10,7 +14,7 @@ async function upload() {
     let transaction = await arweave.createTransaction({
         data: 'x' // FIXME: Upload the actual file.
     }, key);
-    console.log(transaction.id); // the uploaded file
+    console.log("Uploaded file hash:", transaction.id);
     await arweave.transactions.sign(transaction, key);
     const response = await arweave.transactions.post(transaction);
     console.log(response)
@@ -18,4 +22,12 @@ async function upload() {
         alert("Failed ArWeave transaction.");
         return;
     }
+
+    const contractInstance = new web3.eth.Contract(await categoriesJsonInterface(), categoriesContractAddress);
+    contractInstance.methods.uploadFile(itemId,
+                                        document.getElementById('version').value,
+                                        document.getElementById('format').value,
+                                        transaction.id)
+        .send({from: defaultAccount, gas: '1000000'})
+        .then(() => open('description.html?id=' + itemId));
 }
