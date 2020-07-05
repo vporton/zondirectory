c7fc48d1ada4c1f99ef86a36c24a604afea3d1ae
Victor Porton <porton@narod.ru>
2020-07-05 21:06:49 +0300

    Ethereum PST token

--- eth/contracts/BaseToken.sol
+++ eth/contracts/BaseToken.sol
@@ -8,7 +8,7 @@ import "./Token.sol";
 
 contract BaseToken is Token {
 
-    function transfer(address _to, uint256 _value) override external returns (bool success) {
+    function transfer(address _to, uint256 _value) override virtual external returns (bool success) {
         require(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]);
         balances[msg.sender] -= _value;
         balances[_to] += _value;
@@ -16,7 +16,7 @@ contract BaseToken is Token {
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) override external returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) override virtual external returns (bool success) {
         require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]);
         balances[_to] += _value;
         balances[_from] -= _value;
@@ -25,17 +25,17 @@ contract BaseToken is Token {
         return true;
     }
 
-    function balanceOf(address _owner) view override external returns (uint256 balance) {
+    function balanceOf(address _owner) view override virtual external returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function approve(address _spender, uint256 _value) override external returns (bool success) {
+    function approve(address _spender, uint256 _value) override virtual external returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner, address _spender) override external view returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) override virtual external view returns (uint256 remaining) {
       return allowed[_owner][_spender];
     }
 
