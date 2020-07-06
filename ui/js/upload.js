2aa4c2eacb5c5d4802526c52dd2ac88c412d38cb
Victor Porton <porton@narod.ru>
2020-07-06 22:48:44 +0300

    actually upload files

--- ui/js/upload.js
+++ ui/js/upload.js
@@ -3,29 +3,35 @@ async function upload() {
     const itemId = urlParams.get('id');
     if(!itemId) return; // just to be sure
 
-    const fileReader = new FileReader();
-    fileReader.onload = async (e) => {
+    const keyFileReader = new FileReader();
+    keyFileReader.onload = async (e) => {
         const key = JSON.parse(e.target.result);
 
         const arweave = Arweave.init();
-        let transaction = await arweave.createTransaction({
-            data: 'x' // FIXME: Upload the actual file.
-        }, key);
-        await arweave.transactions.sign(transaction, key);
-        const response = await arweave.transactions.post(transaction);
-        if(response.status != 200) {
-            alert("Failed ArWeave transaction.");
-            return;
-        }
+        const fileReader = new FileReader();
+        fileReader.onload = async (e) => {
+            const fileContent = e.target.result;
+            let transaction = await arweave.createTransaction({
+                data: fileContent,
+            }, key);
+            transaction.addTag('Content-Type', document.getElementById('file').files[0].type);
+            await arweave.transactions.sign(transaction, key);
+            const response = await arweave.transactions.post(transaction);
+            if(response.status != 200) {
+                alert("Failed ArWeave transaction.");
+                return;
+            }
 
-        const contractInstance = new web3.eth.Contract(await filesJsonInterface(), filesContractAddress);
-        console.log(transaction.id);
-        contractInstance.methods.uploadFile(itemId,
-                                            document.getElementById('version').value,
-                                            document.getElementById('format').value,
-                                            transaction.id)
-            .send({from: defaultAccount, gas: '1000000'})
-            .then(() => open('description.html?id=' + itemId));
+            const contractInstance = new web3.eth.Contract(await filesJsonInterface(), filesContractAddress);
+            console.log(transaction.id);
+            contractInstance.methods.uploadFile(itemId,
+                                                document.getElementById('version').value,
+                                                document.getElementById('format').value,
+                                                transaction.id)
+                .send({from: defaultAccount, gas: '1000000'})
+                .then(() => open('description.html?id=' + itemId));
+        }
+        fileReader.readAsText(document.getElementById('file').files[0]);
     }
-    fileReader.readAsText(document.getElementById('arWalletKeyFile').files[0]);
+    keyFileReader.readAsText(document.getElementById('arWalletKeyFile').files[0]);
 }
