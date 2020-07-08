55abc934a423a663166b9f8085745e424296cc2a
Victor Porton <porton@narod.ru>
2020-07-08 14:13:55 +0300

    show spam score

--- ui/js/list.js
+++ ui/js/list.js
@@ -15,15 +15,21 @@ async function onLoad() {
     title
     priceETH
     priceAR
+}
+spam${itemId}: votes(first:1, orderBy:id, orderDirection:desc, where:{child:${itemId}, parent:${addressSpam}}) {
+    value
 }`
     }
     query = "{\n" + itemIdsFlat.map(i => subquery(i)).join("\n") + "\n}";
     let items = (await queryThegraph(query)).data;
-    console.log(items)
+    // TODO: Sort on spam score.
     for(let i in items) {
+        if(!/^item/.test(i)) continue;
         const item = items[i][0];
+        const spamInfo = items[i.replace(/^item/, 'spam')][0];
+        const spamScore = spamInfo ? formatPriceETH(-spamInfo.value) : 0;
         const link = "download.html?id=" + item.itemId;
-        const row = `<tr><td><a href="${link}">${safe_tags(item.title)}</a></td><td>${formatPriceETH(item.priceETH)}</td><td>${formatPriceAR(item.priceAR)}</td></tr>`;
+        const row = `<tr><td><a href="${link}">${safe_tags(item.title)}</a></td><td>${formatPriceETH(item.priceETH)}</td><td>${formatPriceAR(item.priceAR)}</td><td>${spamScore}</tr>`;
         $('#theTable').prepend(row);
     }
 }
