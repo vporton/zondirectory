a5d65b87c0813b3270cfd85c7f638b576b888bfd
Victor Porton <porton@narod.ru>
2020-07-08 15:24:22 +0300

    purchasing voting rights

--- ui/js/purchase-voting.js
+++ ui/js/purchase-voting.js
@@ -2,8 +2,20 @@ async function onLoad() {
     await defaultAccountPromise();
     const contractInstance = new web3.eth.Contract(await filesJsonInterface(), addressFiles);    
     contractInstance.methods.balanceOf(defaultAccount).call({from: defaultAccount, gas: '10000000'}, function(error, result){
-        $('#value').html(result == 0 ? '<strong>zero</strong>' : result);
+        $('#value').html(result == 0 ? '<strong>zero</strong>' : formatPriceETH(result));
     });
+}
+
+async function buyVotingRight() {
+    const amount = prompt("Enter amount in ETH:", '0.1');
+    if(!amount) return;
+
+    await defaultAccountPromise();
+    //const contractInstance = new web3.eth.Contract(await filesJsonInterface(), addressFiles);
+    web3.eth.sendTransaction({to: addressFiles, from: defaultAccount, value: web3.utils.toWei(amount)}, (error, result) => {
+        alert("Thank you for your purchase!")
+    });
+
 }
 
 $(onLoad);
