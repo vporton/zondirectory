c87442a5bdfa58f76d7330923c00959e66368c4a
Victor Porton <porton@narod.ru>
2020-07-08 16:49:45 +0300

    vote.html

--- ui/js/create-category.js
+++ ui/js/create-category.js
@@ -1,12 +1,22 @@
+async function onLoad() {
+    await defaultAccountPromise();
+    const contractInstance = new web3.eth.Contract(await filesJsonInterface(), addressFiles);    
+    contractInstance.methods.balanceOf(defaultAccount).call({from: defaultAccount, gas: '10000000'}, function(error, result){
+        $('#value').html(result == 0 ? '<strong>zero</strong>' : formatPriceETH(result));
+    });
+}
+
 async function createCategory() {
     const name = $("#title").val();
     if(!name) return;
 
     await defaultAccountPromise();
     const contractInstance = new web3.eth.Contract(await filesJsonInterface(), addressFiles);
-    // TODO: Wait for a confirmation, open('add-to-category.html?id=...') page.
+    // TODO: Wait for a confirmation, open('vote.html?id=...') page.
     await contractInstance.methods.createCategory(name, 'en').send({from: defaultAccount, gas: '10000000'}, (error, result) => {
         if(error) return;
         alert("Now add this category as a child to another category(ies).");
     });
 }
+
+$(onLoad);
