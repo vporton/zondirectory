e199b42fc790d558785b2ebebf627e894c47e75e
Victor Porton <porton@narod.ru>
2020-07-10 11:37:21 +0300

    lib/ was in .gitignore

---
+++ eth/lib/mydeploy.js
@@ -0,0 +1,39 @@
+"strict";
+
+const fs = require('fs');
+
+function readJson(filename) {
+    try {
+        const buffer = fs.readFileSync(filename);
+        return JSON.parse(buffer);
+    }
+    catch(e) {
+        return {};
+    }
+}
+
+function updateAddress(name, address, network) {
+    try {
+        fs.mkdirSync('data');
+    }
+    catch(e) { }
+
+    const filename1 = 'config/' + network + '.addresses';
+    const filename2 = 'data/' + network + '.addresses';
+
+    let data1 = readJson(filename1);
+    let data2 = readJson(filename2);
+
+    for(let key in data2) data1[key] = data2[key];
+    data1[name] = address;
+
+    fs.writeFileSync(filename2, JSON.stringify(data1, null, 2));
+
+    let js = "";
+    for(let key in data1)
+        js += "const address" + key + " = '" + data1[key] + "';\n";
+    fs.mkdirSync('../out/artifacts', { recursive: true });
+    fs.writeFileSync('../out/artifacts/addresses.js', js);
+}
+
+module.exports = { updateAddress };
---
+++ lib/format.xslt
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:xi="http://www.w3.org/2001/XInclude"
+                xmlns:html="http://www.w3.org/1999/xhtml"
+                version="1.0">
+
+    <xsl:output method="xml" version="1.1"/>
+
+    <xsl:param name="input"/>
+
+    <xsl:variable name="doc" select="document($input)"/>
+
+    <xsl:template match="node()|@*">
+        <xsl:copy>
+            <xsl:apply-templates select="node()|@*"/>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="html:*[not(node())]">
+        <xsl:copy>
+            <xsl:apply-templates select="@*"/>
+            <xsl:text> </xsl:text>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="xi:include">
+        <xsl:variable name="id" select="substring(@href,2)"/>
+        <xsl:apply-templates select="$doc//*[@id=$id]/node()"/>
+    </xsl:template>
+
+</xsl:stylesheet>
