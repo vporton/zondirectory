016649216a05fd1c69db969a1a5521dcadaf0d11
Victor Porton <porton@narod.ru>
2020-07-10 20:58:31 +0300

    bug fix

--- ui/js/list.js
+++ ui/js/list.js
@@ -65,59 +65,60 @@ async function onLoad() {
     $('#subcategories > li:gt(9)').css('display', 'none');
 
     let itemIds = queryResult[catId ? 'childParentVotes' : 'itemCreateds'];
-    if(!itemIds.length) return;
     if(queryResult.categoryCreateds) {
         const categoryTitle = queryResult.categoryCreateds[0].title;
         $('#catTitle').text(categoryTitle);
     }
-    const itemIdsFlat = catId ? itemIds.map(i => i.child) : itemIds.map(i => i.itemId);
-    // TODO: Don't request category title if not asked for category votes.
-    function subquery(itemId) {
-        let query = `    item${itemId}: itemUpdateds(first:1, orderBy:id, orderDirection:desc, where:{itemId:${itemId}}) {
-        itemId # TODO: Superfluous
-        title
-        priceETH
-        priceAR
-    }
-    category${itemId}: categoryCreateds(first:1, orderBy: id, orderDirection:asc, where:{categoryId:${itemId}}) {
-        title
-    }`
-        if(catId) {
-            query += `
-            spam${itemId}: childParentVotes(first:1, orderBy:id, orderDirection:desc, where:{child:${itemId}, parent:${catId}}) {
-                value
-            }`;
+    if(itemIds.length) {
+        const itemIdsFlat = catId ? itemIds.map(i => i.child) : itemIds.map(i => i.itemId);
+        // TODO: Don't request category title if not asked for category votes.
+        function subquery(itemId) {
+            let query = `    item${itemId}: itemUpdateds(first:1, orderBy:id, orderDirection:desc, where:{itemId:${itemId}}) {
+            itemId # TODO: Superfluous
+            title
+            priceETH
+            priceAR
         }
-        return query;
-    }
-    query = "{\n" + itemIdsFlat.map(i => subquery(i)).join("\n") + "\n}";
-    let items = (await queryThegraph(query)).data;
-    // TODO: Sort on spam score.
-    if(catId)
+        category${itemId}: categoryCreateds(first:1, orderBy: id, orderDirection:asc, where:{categoryId:${itemId}}) {
+            title
+        }`
+            if(catId) {
+                query += `
+                spam${itemId}: childParentVotes(first:1, orderBy:id, orderDirection:desc, where:{child:${itemId}, parent:${catId}}) {
+                    value
+                }`;
+            }
+            return query;
+        }
+        query = "{\n" + itemIdsFlat.map(i => subquery(i)).join("\n") + "\n}";
+        let items = (await queryThegraph(query)).data;
+        // TODO: Sort on spam score.
+        if(catId)
+            for(let i in items) {
+                if(!/^category/.test(i)) continue;
+                const category = items[i][0];
+                if(!category) continue;
+                const spamInfo = items[i.replace(/^category/, 'spam')][0];
+                const spamScore = spamInfo ? formatPriceETH(spamInfo.value) : 0;
+                const link = "index.html?cat=" + i.replace(/^category/, "");
+                $('#subcategoies').append(`<li><a href="${link}">${safe_tags(category.title)}</a> (spam score: `-spamScore`)</li>`);
+            }
         for(let i in items) {
-            if(!/^category/.test(i)) continue;
-            const category = items[i][0];
-            if(!category) continue;
-            const spamInfo = items[i.replace(/^category/, 'spam')][0];
-            const spamScore = spamInfo ? formatPriceETH(spamInfo.value) : 0;
-            const link = "index.html?cat=" + i.replace(/^category/, "");
-            $('#subcategoies').append(`<li><a href="${link}">${safe_tags(category.title)}</a> (spam score: `-spamScore`)</li>`);
-        }
-    for(let i in items) {
-        if(!/^item/.test(i)) continue;
-        const item = items[i][0];
-        if(!item) continue;
-        const link = "download.html?id=" + item.itemId;
-        if(catId) {
-            const spamInfo = items[i.replace(/^item/, 'spam')][0];
-            const spamScore = spamInfo ? formatPriceETH(-spamInfo.value) : 0;
-            const voteStr = `<a href='vote.html?child=${i.replace(/^item/, "")}&parent=${catId}&dir=for'>👍</a>` +
-                `<a href='vote.html?child=${i.replace(/^item/, "")}&parent=${catId}&dir=against'>👎</a>`;
-            const row = `<tr><td><a href="${link}">${safe_tags(item.title)}</a></td><td>${formatPriceETH(item.priceETH)}</td><td>${formatPriceAR(item.priceAR)}</td><td>${spamScore} voteStr</tr>`;
-            $('#theTable').prepend(row);
-        } else {
-            const row = `<tr><td><a href="${link}">${safe_tags(item.title)}</a></td><td>${formatPriceETH(item.priceETH)}</td><td>${formatPriceAR(item.priceAR)}</td>`;
-            $('#theTable').prepend(row);
+            if(!/^item/.test(i)) continue;
+            const item = items[i][0];
+            if(!item) continue;
+            const link = "download.html?id=" + item.itemId;
+            if(catId) {
+                const spamInfo = items[i.replace(/^item/, 'spam')][0];
+                const spamScore = spamInfo ? formatPriceETH(-spamInfo.value) : 0;
+                const voteStr = `<a href='vote.html?child=${i.replace(/^item/, "")}&parent=${catId}&dir=for'>👍</a>` +
+                    `<a href='vote.html?child=${i.replace(/^item/, "")}&parent=${catId}&dir=against'>👎</a>`;
+                const row = `<tr><td><a href="${link}">${safe_tags(item.title)}</a></td><td>${formatPriceETH(item.priceETH)}</td><td>${formatPriceAR(item.priceAR)}</td><td>${spamScore} voteStr</tr>`;
+                $('#theTable').prepend(row);
+            } else {
+                const row = `<tr><td><a href="${link}">${safe_tags(item.title)}</a></td><td>${formatPriceETH(item.priceETH)}</td><td>${formatPriceAR(item.priceAR)}</td>`;
+                $('#theTable').prepend(row);
+            }
         }
     }
 }
