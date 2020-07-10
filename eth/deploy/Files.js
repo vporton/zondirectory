5dc9fc3cae96b0ed16b346cb3e6b8cc10aa10c59
Victor Porton <porton@narod.ru>
2020-07-10 20:47:17 +0300

    deployed Rinkeby

--- eth/deploy/Files.js
+++ eth/deploy/Files.js
@@ -10,17 +10,20 @@ function filesJsonInterface() {
 
 let categories = {};
 
-async function createCategory(address, name) {
+async function createCategory(address, blockNumber, name) {
     const contractInstance = new web3.eth.Contract(filesJsonInterface(), address);
     const namedAccounts = await getNamedAccounts();
-    const {deployer} = namedAccounts;   
+    const {deployer} = namedAccounts;
     await contractInstance.methods.createCategory(name, 'en').send({from: deployer, gas: '1000000'})
         .on('error', (error) => log(`Error creating category: ` + error))
         .catch((error) => log(`Error creating category: ` + error));
-    categories[name] = await getCategoryId(address, name, contractInstance);
+    log(`created category "${name}"...`);
+    // It does not work on Infura Rinkeby as of 10 Jul 2020:
+    // categories[name] = await getCategoryId(address, blockNumber, name, contractInstance);
+    // log(`category ID ${await categories[name]}`);
 }
 
-function getCategoryId(address, name, contractInstance) {
+function getCategoryId(address, blockNumber, name, contractInstance) {
     return new Promise((resolve, errorHandler) => {
         // Error: The current provider doesn't support subscriptions: Web3HTTPProviderAdapter
         // contractInstance.once('CategoryCreated', {
@@ -34,12 +37,12 @@ function getCategoryId(address, name, contractInstance) {
         // .catch((error) => log(`Error getting category ID: ` + error));
         const readEvents = function() {
             contractInstance.getPastEvents('CategoryCreated', {
-                filter: {title: name, locale: 'en'},
-                fromBlock: 0,
+                filter: {/*title: name, locale: 'en'*/},
+                fromBlock: blockNumber,
                 toBlock: 'pending',
             })
                 .then(function(events) {
-                    // log(events)
+                    log(events)
                     for(let i in events) {
                         const event = events[i];
                         if(event.event == 'CategoryCreated') {
@@ -47,7 +50,7 @@ function getCategoryId(address, name, contractInstance) {
                             return;
                         }
                     }
-                    setTimeout(readEvents, 100);
+                    setTimeout(readEvents, 10000);
                 })
                 .catch((error) => errorHandler(`Error getting category ID: ` + error));
         }
@@ -71,35 +74,39 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     log(`Deploying Files...`);
     const deployResult = await deploy('Files', {from: deployer, args: [process.env.PROGRAMMER_ADDRESS, 10000000]});
     if (deployResult.newlyDeployed) {
-        log(`contract Files deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`);
+        log(`contract Files deployed at ${deployResult.address} in block ${deployResult.receipt.blockNumber} using ${deployResult.receipt.gasUsed} gas`);
 
         log(`Creating categories...`);
-        await createCategory(deployResult.address, "Root");
-        await createCategory(deployResult.address, "Spam");
-        await createCategory(deployResult.address, "E-books");
-        await createCategory(deployResult.address, "Videos");
-        await createCategory(deployResult.address, "Software");
-        await createCategory(deployResult.address, "Binaries");
-        await createCategory(deployResult.address, "Sources");
-        var categoryNames = Object.keys(categories);
-        var allCategories = categoryNames.map(v => categories[v]);
-        Promise.all(allCategories);
-        //console.log(await Promise.all(categoryNames.map(async v => await categories[v])));
-        log(`created ${allCategories.length} categories`);
+        await createCategory(deployResult.address, deployResult.receipt.blockNumber, "Root");
+        await createCategory(deployResult.address, deployResult.receipt.blockNumber, "Spam");
+        await createCategory(deployResult.address, deployResult.receipt.blockNumber, "E-books");
+        await createCategory(deployResult.address, deployResult.receipt.blockNumber, "Videos");
+        await createCategory(deployResult.address, deployResult.receipt.blockNumber, "Software");
+        await createCategory(deployResult.address, deployResult.receipt.blockNumber, "Binaries");
+        await createCategory(deployResult.address, deployResult.receipt.blockNumber, "Sources");
+        // It does not work on Infura, see above:
+        // var categoryNames = Object.keys(categories);
+        // var allCategories = categoryNames.map(v => categories[v]);
+        // Promise.all(allCategories);
+        // //console.log(await Promise.all(categoryNames.map(async v => await categories[v])));
+        // log(`created ${allCategories.length} categories`);
 
-        log(`Creating category relations...`)
-        addItemToCategory(await categories["Root"], await categories["E-books"]);
-        addItemToCategory(await categories["Root"], await categories["Videos"]);
-        addItemToCategory(await categories["Root"], await categories["Software"]);
-        addItemToCategory(await categories["Software"], await categories["Binaries"]);
-        addItemToCategory(await categories["Software"], await categories["Sources"]);
-        log(`created base category structure`);
+        // log(`Creating category relations...`)
+        // addItemToCategory(await categories["Root"], await categories["E-books"]);
+        // addItemToCategory(await categories["Root"], await categories["Videos"]);
+        // addItemToCategory(await categories["Root"], await categories["Software"]);
+        // addItemToCategory(await categories["Software"], await categories["Binaries"]);
+        // addItemToCategory(await categories["Software"], await categories["Sources"]);
+        // log(`created base category structure`);
     }
     const mydeploy = require('../lib/mydeploy');
     mydeploy.updateAddress('Files', deployResult.address, buidler.network.name); // or ethers.getContractAt
-    if(await categories["Root"])
-        mydeploy.updateAddress('Root', await categories["Root"], buidler.network.name);
-    if(await categories["Spam"])
-        mydeploy.updateAddress('Spam', await categories["Spam"], buidler.network.name);
+    mydeploy.updateAddress('FilesBlock', deployResult.receipt.blockNumber, buidler.network.name); // or ethers.getContractAt
+    // if(await categories["Root"])
+    //     mydeploy.updateAddress('Root', await categories["Root"], buidler.network.name);
+    // if(await categories["Spam"])
+    //     mydeploy.updateAddress('Spam', await categories["Spam"], buidler.network.name);
+    mydeploy.updateAddress('Root', 1, buidler.network.name);
+    mydeploy.updateAddress('Spam', 2, buidler.network.name);
 }
 module.exports.tags = ['Files'];
