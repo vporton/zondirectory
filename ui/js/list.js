2c0c545ecadddf418401e929affbb191676753c4
Victor Porton <porton@narod.ru>
2020-07-11 13:47:20 +0300

    show categories list

--- ui/js/list.js
+++ ui/js/list.js
@@ -46,31 +46,25 @@ async function onLoad() {
     for(let i in queryResult.parents) {
         const entry = queryResult.parents[i];
         if(!parents.has(i) || parents.get[i].id > entry.id)
-            parents.set(i, {id: entry.id, value: entry.value})
+            parents.set(i, {id: entry.id, parent: entry.parent, value: entry.value})
     }
     let childs = new Map();
     for(let i in queryResult.childs) {
         const entry = queryResult.childs[i];
         if(!childs.has(i) || childs.get[i].id > entry.id)
-            childs.set(i, {id: entry.id, value: entry.value})
+            childs.set(i, {id: entry.id, child: entry.child, value: entry.value})
     }
-    for(let parent in Array.from(parents.values()).sort((a, b) => b.value - a.value)) {
-        $('#supercategories').append(`<li><a href="index.html?cat=${parent.parent}">${parent.title}</a></li>`);
-    }
-    for(let child in Array.from(childs.values()).sort((a, b) => b.value - a.value)) {
-        $('#subcategories').append(`<li><a href="index.html?cat=${child.parent}">${child.title}</a></li>`);
-    }
-    
-    $('#supercategories > li:gt(0)').css('display', 'none');
-    $('#subcategories > li:gt(9)').css('display', 'none');
+    const parentIDs = Array.from(parents.values()).sort((a, b) => b.value - a.value).map(e => e.parent);
+    const childIDs = Array.from(childs.values()).sort((a, b) => b.value - a.value).map(e => e.child);
 
-    let itemIds = queryResult[catId ? 'childParentVotes' : 'itemCreateds'];
     if(queryResult.categoryCreateds) {
         const categoryTitle = queryResult.categoryCreateds[0].title;
         $('#catTitle').text(categoryTitle);
     }
-    if(itemIds.length) {
-        const itemIdsFlat = catId ? itemIds.map(i => i.child) : itemIds.map(i => i.itemId);
+    const itemIds = queryResult['itemCreateds'] ? queryResult['itemCreateds'][0] : [];
+    const entryIdsFlat = catId ? parentIDs.concat(childIDs, itemIds.map(i => i.child))
+                              : itemIds.map(i => i.itemId);
+    if(entryIdsFlat.length) {
         // TODO: Don't request category title if not asked for category votes.
         function subquery(itemId) {
             let query = `    item${itemId}: itemUpdateds(first:1, orderBy:id, orderDirection:desc, where:{itemId:${itemId}}) {
@@ -83,37 +77,42 @@ async function onLoad() {
             title
         }`
             if(catId) {
+                // TODO: rspam seems sometimes unnecessary.
                 query += `
                 spam${itemId}: childParentVotes(first:1, orderBy:id, orderDirection:desc, where:{child:${itemId}, parent:${catId}}) {
+                    value
+                }
+                rspam${itemId}: childParentVotes(first:1, orderBy:id, orderDirection:desc, where:{parent:${itemId}, child:${catId}}) {
                     value
                 }`;
             }
             return query;
         }
-        query = "{\n" + itemIdsFlat.map(i => subquery(i)).join("\n") + "\n}";
+        query = "{\n" + entryIdsFlat.map(i => subquery(i)).join("\n") + "\n}";
         let items = (await queryThegraph(query)).data;
-        // TODO: Sort on spam score.
         if(catId) {
-            console.log(parents)
-            for(let i in parents) {
-                if(!/^category/.test(i)) continue;
-                const category = parents[i][0];
+            for(let i in parentIDs) {
+                const categoryId = parentIDs[i];
+                const category = items['category' + categoryId][0];
                 if(!category) continue;
-                const spamInfo = items[i.replace(/^category/, 'spam')][0];
-                const spamScore = spamInfo ? formatPriceETH(spamInfo.value) : 0;
-                const link = "index.html?cat=" + i.replace(/^category/, "");
-                $('#subcategoies').append(`<li><a href="${link}">${safe_tags(category.title)}</a> (spam score: `-spamScore`)</li>`);
+                const spamInfo = items['rspam' + categoryId][0];
+                const spamScore = spamInfo ? formatPriceETH(new web3.utils.BN(spamInfo.value).neg()) : 0;
+                const link = "index.html?cat=" + categoryId;
+                $('#supercategories').append(`<li><a href="${link}">${safe_tags(category.title)}</a> (spam score: ${spamScore})</li>`);
             }
-            for(let i in childs) {
-                if(!/^category/.test(i)) continue;
-                const category = childs[i][0];
+            for(let i in childIDs) {
+                const categoryId = childIDs[i];
+                const category = items['category' + categoryId][0];
                 if(!category) continue;
-                const spamInfo = items[i.replace(/^category/, 'spam')][0];
-                const spamScore = spamInfo ? formatPriceETH(spamInfo.value) : 0;
-                const link = "index.html?cat=" + i.replace(/^category/, "");
-                $('#subcategoies').append(`<li><a href="${link}">${safe_tags(category.title)}</a> (spam score: `-spamScore`)</li>`);
+                const spamInfo = items['spam' + categoryId][0];
+                const spamScore = spamInfo ? formatPriceETH(new web3.utils.BN(spamInfo.value).neg()) : 0;
+                const link = "index.html?cat=" + categoryId;
+                $('#subcategories').append(`<li><a href="${link}">${safe_tags(category.title)}</a> (spam score: ${spamScore})</li>`);
             }
-        }
+    
+            $('#supercategories > li:gt(0)').css('display', 'none');
+            $('#subcategories > li:gt(9)').css('display', 'none');
+                }
         for(let i in items) {
             if(!/^item/.test(i)) continue;
             const item = items[i][0];
