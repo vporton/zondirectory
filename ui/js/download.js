5eaf9467f1ce5cf7e517a77059e0f33a07745ea4
Victor Porton <porton@narod.ru>
2020-07-12 14:58:05 +0300

    allow owned categories and owned entries to be displayed

--- ui/js/download.js
+++ ui/js/download.js
@@ -175,7 +175,7 @@ $(async function() {
         if(parentIDs) {
             function subquery(catId) {
                 let query = `
-            category${catId}: categoryCreateds(first:1, orderBy: id, orderDirection:asc, where:{categoryId:${catId}, owner:0}) {
+            category${catId}: categoryCreateds(first:1, orderBy: id, orderDirection:asc, where:{categoryId:${catId}}) {
                 title
             }`
                 query += `
