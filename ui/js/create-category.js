fbf72a9543b142a3e44ed8392e423861286fd92e
Victor Porton <porton@narod.ru>
2020-07-12 15:19:13 +0300

    more options for category creation

--- ui/js/create-category.js
+++ ui/js/create-category.js
@@ -9,11 +9,14 @@ async function onLoad() {
 async function createCategory() {
     const name = $("#title").val();
     if(!name) return;
+    const locale = $("#locale").val();
+    if(!locale) return;
+    const owned = $('#owned').is(':checked');
 
     await defaultAccountPromise();
     const contractInstance = new web3.eth.Contract(await filesJsonInterface(), addressFiles);
     // TODO: Wait for a confirmation, open('vote.html?id=...') page.
-    await contractInstance.methods.createCategory(name, 'en', false).send({from: defaultAccount, gas: '10000000'}, (error, result) => {
+    await contractInstance.methods.createCategory(name, locale, owned).send({from: defaultAccount, gas: '10000000'}, (error, result) => {
         if(error) return;
         alert("Now add this category as a child to another category(ies).");
     });
