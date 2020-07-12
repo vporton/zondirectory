6c291146d76771dd91a1c2498958b3d3c58dfd48
Victor Porton <porton@narod.ru>
2020-07-12 15:03:22 +0300

    UI for creating categories

--- ui/js/create-category.js
+++ ui/js/create-category.js
@@ -13,7 +13,7 @@ async function createCategory() {
     await defaultAccountPromise();
     const contractInstance = new web3.eth.Contract(await filesJsonInterface(), addressFiles);
     // TODO: Wait for a confirmation, open('vote.html?id=...') page.
-    await contractInstance.methods.createCategory(name, 'en').send({from: defaultAccount, gas: '10000000'}, (error, result) => {
+    await contractInstance.methods.createCategory(name, 'en', false).send({from: defaultAccount, gas: '10000000'}, (error, result) => {
         if(error) return;
         alert("Now add this category as a child to another category(ies).");
     });
