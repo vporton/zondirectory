dcbd021c8ede4bc714ff6348b94e2827223d69e4
Victor Porton <porton@narod.ru>
2020-07-13 18:28:41 +0300

    creating entries to test

--- eth/test/Files.js
+++ eth/test/Files.js
@@ -5,7 +5,16 @@ describe("Files", function() {
   it("Dividends", async function() {
     const Files = await ethers.getContractFactory("Files");
     const files = await Files.deploy(process.env.PROGRAMMER_ADDRESS, 10000000);
-    
+
     await files.deployed();
+
+    await files.createCategory("Owned category", "en", true);
+    await files.createCategory("Unowned category", "en", false);
+    await files.createItem("Item 1",
+                           "xxx",
+                           web3.utils.toWei('2.0'),
+                           1, // ignore it
+                           'en',
+                           'commercial');
   });
 });
