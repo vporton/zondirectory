26660bbb4fafc2edf63f84fa6f68a2ba58019834
Victor Porton <porton@narod.ru>
2020-07-13 22:30:16 +0300

    bug fixes in test

--- eth/test/Files.js
+++ eth/test/Files.js
@@ -6,20 +6,24 @@ async function extractEvent(response, eventName) {
   return receipt.events.filter(log => log.event == eventName)[0].args
 }
 
+function myToWei(n) {
+  return ethers.utils.parseEther(String(n));
+}
+
 describe("Files", function() {
   it("Dividends", async function() {
     const [deployer, founder, partner, seller, buyer] = await ethers.getSigners();
 
-    const PARTNER_PERCENT = '30';
-    const FIRST_PURCHASE = '3.535';
-    const SECOND_PURCHASE = '2.335';
-    const VOTE_AMOUNT = '1.22';
+    const PARTNER_PERCENT = 30;
+    const FIRST_PURCHASE = 3.535;
+    const SECOND_PURCHASE = 2.335;
+    const VOTE_AMOUNT = 1.22;
 
     const Files = await ethers.getContractFactory("Files");
-    const files = await Files.deploy(await founder.getAddress(), web3.utils.toWei('100'));
+    const files = await Files.deploy(await founder.getAddress(), myToWei('100'));
     await files.deployed();
 
-    files.connect(founder).transfer(await partner.getAddress(), web3.utils.toWei(PARTNER_PERCENT));
+    files.connect(founder).transfer(await partner.getAddress(), myToWei(PARTNER_PERCENT));
 
     const ownedCategoryId = (await extractEvent(files.connect(seller).createCategory("Owned category", "en", true), 'CategoryCreated')).categoryId;
     const unownedCategoryId = (await extractEvent(files.connect(seller).createCategory("Unowned category", "en", false), 'CategoryCreated')).categoryId;
@@ -30,8 +34,15 @@ describe("Files", function() {
                   1, // ignore it
                   'en',
                   'commercial'), 'ItemCreated')).itemId;
-    await files.connect(buyer).pay(itemId, {value: web3.utils.toWei(FIRST_PURCHASE)})
-    await files.connect(buyer).vote(itemId, ownedCategoryId, {value: web3.utils.toWei(VOTE_AMOUNT)});
-    await files.connect(buyer).vote(itemId, unownedCategoryId, {value: web3.utils.toWei(VOTE_AMOUNT)});
+    await files.connect(buyer).pay(itemId, {value: myToWei(FIRST_PURCHASE)})
+    await files.connect(buyer).voteChildParent(itemId, ownedCategoryId, {value: myToWei(VOTE_AMOUNT)});
+    await files.connect(buyer).voteChildParent(itemId, unownedCategoryId, {value: myToWei(VOTE_AMOUNT)});
+
+    // TODO: Test setting fees.
+    const totalDividend1 = (FIRST_PURCHASE + SECOND_PURCHASE) * 0.1 + VOTE_AMOUNT * 0.5;
+    const founderDividend1 = await files.dividendsOwing(await founder.getAddress());
+    const expectedFounderDividend1 = totalDividend1 * (100 - PARTNER_PERCENT) / 100;
+    console.log(ethers.utils.formatEther(founderDividend1), expectedFounderDividend1);
+    //console.log(Number(founderDividend1.toString()) / Number(expectedFounderDividend1.toString()));
   });
 });
