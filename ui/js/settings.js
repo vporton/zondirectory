22e577f2f4038c9a66a74b71d39bf185c881b6bc
Victor Porton <porton@narod.ru>
2020-07-26 10:42:18 +0300

    improved cookie security

--- ui/js/settings.js
+++ ui/js/settings.js
@@ -2,7 +2,8 @@
 
 function networkChange() {
     const network = $('#web3network').val();
-    document.cookie = `web3network=${network}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    //const path = location.pathname.replace(/[^/\\]*$/, "");
+    document.cookie = `web3network=${network}; SameSite=Strict; Secure; Expires=Fri, 31 Dec 9999 23:59:59 GMT`;
 }
 
 function onLoad() {
