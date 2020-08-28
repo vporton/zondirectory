0dd26a29496d552877ab04d99d87eec539e7e64e
Victor Porton <porton@narod.ru>
2020-08-28 20:54:25 +0300

    bug fix

--- ui/js/player.js
+++ ui/js/player.js
@@ -1,17 +1,20 @@
 function displayVideo(id, link) {
+    link = link.replace(/^arweave:/, "https://arweave.net/");
     $.ajax(link, {method: 'HEAD'})
         .then(function(data, textStatus, xhr) {
             const type = xhr.getResponseHeader("content-type") || "";
             if(type.startsWith('video/')) {
-                const source = $(id + 'video > source');
+                const source = $(`#${id}video source`);
                 source.attr('src', link);
                 source.attr('type', type);
-                $(id + 'video').css('display', 'block');
+                $(`#${id}video`)[0].load();
+                $(`#${id}video`).css('display', 'block');
             } else if(type.startsWith('audio/') || type == 'application/x-mpegURL') {
-                const source = $(id + 'audio > source');
+                const source = $(`#${id}audio source`);
                 source.attr('src', link);
                 source.attr('type', type);
-                $(id + 'audio').css('display', 'block');
+                $(`#${id}audio`)[0].load();
+                $(`#${id}audio`).css('display', 'block');
             }
         })
 }
