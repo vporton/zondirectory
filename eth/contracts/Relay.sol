a79b46e0f55fc8426841ba1ebebb1e1352d7458d
Victor Porton <porton@narod.ru>
2020-09-02 23:07:52 +0300

    Relay.sol

---
+++ eth/contracts/Relay.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.6.0;
+
+contract Relay {
+    address public currentVersion;
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(address initAddr) public {
+        currentVersion = initAddr;
+        owner = msg.sender; // this owner may be another contract with multisig, not a single contract owner
+    }
+
+    function changeContract(address newVersion) public
+    onlyOwner()
+    {
+        currentVersion = newVersion;
+    }
+
+    fallback() external {
+        address addr = currentVersion;
+        uint size;
+        assembly { size := extcodesize(addr) }
+        require(size > 0);
+
+        (bool success, /*bytes memory result*/) = addr.delegatecall(msg.data);
+        require(!success);
+    }
+}
